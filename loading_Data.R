00ee51bb1e1ec20a217101d12132a02f89210074
cjosmoreno <cjosmoreno@users.noreply.github.com>
2015-08-05 07:56:06 -0700

    Update loading_Data.R

--- loading_Data.R
+++ loading_Data.R
@@ -1,1 +1,9 @@
-
+  setwd("C:/Users/CarlosJosé/Desktop/Data Mining/Jonhs Hopkins/Ciencia de Datos/Exploratory Data Analysis")
+  datos <- "household_power_consumption.txt"
+	cargardatos <- read.table(datos, header=TRUE, sep=";", colClasses=c("character", "character", rep("numeric",7)), na="?")
+	cargardatos$Time <- strptime(paste(cargardatos$Date, cargardatos$Time), "%d/%m/%Y %H:%M:%S")
+	cargardatos$Date <- as.Date(cargardatos$Date, "%d/%m/%Y")
+	fechas <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
+	cargardatos <- subset(cargardatos, Date %in% fechas)
+	datosresumidos <- paste(getwd(), "/", "datos", "/", "datosresumidos", ".rds", sep="")
+	saveRDS(cargardatos, datosresumidos)
